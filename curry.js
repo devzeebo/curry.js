b885a6b20ad886bbe19c37fb6da0224015b9e488
Eric Siebeneich <eric.siebeneich@gmail.com>
2017-01-25 22:55:18 -0600

    Switched to 'call' vs 'apply'. Added bower.json

--- curry.js
+++ curry.js
@@ -1,7 +1,37 @@
-Function.prototype.curry = function() {
-	var func = this;
-	var args = Array.prototype.slice.call(arguments);
-	return function() {
-		return func.apply(this, args.concat(Array.prototype.slice.call(arguments)));
+(function() {
+	var funcDecs = {};
+
+	function funcCall(cArgs, fArgs) {
+		funcDecs[cArgs] = funcDecs[cArgs] || {};
+		if (!funcDecs[cArgs][fArgs]) {
+			var cArr = [];
+			for (var i = 0; i < cArgs; i++) {
+				cArr.push('cArgs[' + i + ']');
+			}
+			var cArgStr = cArr.join(', ');
+			var fArr = [];
+			for (var i = 0; i < fArgs; i++) {
+				fArr.push('fArgs[' + i + ']');
+			}
+			var fArgStr = fArr.join(', ');
+			var evalStr =
+			"[function(_this, func, cArgs, fArgs) {" +
+				"return func.call(_this" + 
+					(cArgs ? ", " + cArgStr : '') +
+					(fArgs ? ", " + fArgStr : '') + ");" +
+			"}]";
+			funcDecs[cArgs][fArgs] = eval(evalStr)[0];
+		}
+
+		return funcDecs[cArgs][fArgs];
 	}
-};
+
+	Function.prototype.curry = function() {
+		var func = this;
+		var args = Array.prototype.slice.call(arguments);
+		var aLen = args.length;
+		return function() {
+			return (funcDecs[aLen] && funcDecs[aLen][arguments.length] || funcCall(aLen, arguments.length))(this, func, args, Array.prototype.slice.call(arguments));
+		}
+	};
+})();
